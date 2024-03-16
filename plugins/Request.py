f9e2c6a554daf3292d3799b8e9a5b1193d1ecb4e
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2024-03-16 18:08:43 +0530

    Update Request.py

--- plugins/Request.py
+++ plugins/Request.py
@@ -1,16 +1,17 @@
 from pyrogram import Client, filters
 from info import API_ID, API_HASH, BOT_TOKEN, ADMINS
+OWNER_ID = ADMINS 
 
-OWNER_ID = ADMINS
+client = Client("royal", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-client = Client("movie_request_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-@client.on_message(filters.command("request") & filters.private)
-async def request_movie(bot, update):
-    if len(update.command) <= 1:
-        await bot.send_message(update.chat.id, "Please provide the name of the movie with the /request command.")
+@client.on_message(filters.command("request"))
+async def request_movie(bot, message):
+    if len(message.command) <= 1:
+        await bot.send_message(message.chat.id, "Please provide the name of the movie with the /request command.")
     else:
-        movie_name = ' '.join(update.command[1:])
+        movie_name = ' '.join(message.command[1:])
         owner_message = f"New movie request: {movie_name}"
         await bot.send_message(OWNER_ID, owner_message)
+        await bot.send_message(message.chat.id, "Movie request sent successfully.")
 
-client.run()
+client.start()
